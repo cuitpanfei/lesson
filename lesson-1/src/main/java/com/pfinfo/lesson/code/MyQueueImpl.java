1d42aeffe6d4862c46a52329d409c6800bd3e7c4
”panfei“ <ys.panfei@h3c.com>
2018-11-07 10:45:14 +0800

    添加代码主类

---
+++ lesson-1/src/main/java/com/pfinfo/lesson/code/MyQueueImpl.java
@@ -0,0 +1,15 @@
+package com.pfinfo.lesson.code;
+
+/**
+ * 代码运行区域
+ * @author pys1714
+ *
+ */
+public class MyQueueImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
